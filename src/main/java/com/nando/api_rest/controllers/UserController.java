89c37eedf16399344da2862b8b300673ef96ac5b
Nando717 <iverlandioedsonn@gmail.com>
2025-01-30 20:52:57 -0300

    atualizando services

--- src/main/java/com/nando/api_rest/controllers/UserController.java
+++ src/main/java/com/nando/api_rest/controllers/UserController.java
@@ -9,10 +9,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/app")
@@ -32,4 +33,57 @@ public class UserController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
     }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> findA (User user){
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAlll(user));
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<Object> findId (@PathVariable(name = "id")UUID id){
+
+        Optional<User> usEr = userRepository.findById(id);
+
+        if (usEr.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("usuario não encontrado");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findById(id));
+
+    }
+
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<Object> upUser (@PathVariable(name = "id") UUID id,
+                                          @RequestBody @Valid UserRecordDto userRecordDto){
+
+        Optional<User> usER = userRepository.findById(id);
+
+        if (usER.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("usuario não encontrado");
+        }
+        var user = usER.get();
+
+        BeanUtils.copyProperties(userRecordDto, user);
+
+        return ResponseEntity.status(HttpStatus.OK).body(userService.save(user));
+    }
+
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<Object> deleteUsers (@PathVariable(name = "id") UUID id){
+
+        Optional<User> usER = userRepository.findById(id);
+
+        if (usER.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("usuario não encontrado");
+
+        }
+
+        userRepository.delete(usER.get());
+        return ResponseEntity.status(HttpStatus.OK).body("usuario deletado");
+    }
+
+
 }
--- src/main/java/com/nando/api_rest/services/UserService.java
+++ src/main/java/com/nando/api_rest/services/UserService.java
@@ -3,9 +3,14 @@ package com.nando.api_rest.services;
 
 import com.nando.api_rest.domains.User;
 import com.nando.api_rest.repositories.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -18,4 +23,30 @@ public class UserService {
     public User save (User user){
         return userRepository.save(user);
     }
+
+    @Transactional
+    public List<User> findAlll(User user){
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    public Optional<User> findById (UUID id){
+        return userRepository.findById(id);
+    }
+
+
+    @Transactional
+    public User apdateUser (UUID id, User user){
+       userRepository.findById(id);
+        return userRepository.save(user);
+    }
+
+
+    @Transactional
+    public User findName (User user){
+        return userRepository.findByName();
+    }
+
+
+
 }
